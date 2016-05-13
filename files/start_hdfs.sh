32e3fcb9fe9c66fea1e802fee67d6bbc0ca7c113
Giannis Mouchakis <gmouchakis@iit.demokritos.gr>
2016-05-13 18:22:06 +0300

    Use bde-cassandra  and bde-hadoop

--- files/start_hdfs.sh
+++ files/start_hdfs.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-docker -H tcp://master:4000 run -d --name namenode  -p 8020:8020 -p 50070:50070 -v /srv/hdfs:/hdfs-data --net=bde_net -e constraint:node==master bde2020/hadoop-namenode
-docker -H tcp://master:4000 run -d --name datanode1 -v /srv/hdfs:/hdfs-data --net=bde_net -e constraint:node==slave1 bde2020/hadoop-datanode
-docker -H tcp://master:4000 run -d --name datanode2 -v /srv/hdfs:/hdfs-data --net=bde_net -e constraint:node==slave2 bde2020/hadoop-datanode
-docker -H tcp://master:4000 run -d --name datanode3 -v /srv/hdfs:/hdfs-data --net=bde_net -e constraint:node==slave3 bde2020/hadoop-datanode
-docker -H tcp://master:4000 run -d --name datanode4 -v /srv/hdfs:/hdfs-data --net=bde_net -e constraint:node==slave4 bde2020/hadoop-datanode
-
+git clone https://github.com/big-data-europe/docker-hadoop.git
+cd docker-hadoop
+docker -H tcp://master:4000 run -d --name namenode -v /srv/hadoop/namenode:/hadoop/dfs/name --net=bde_net -h namenode.hadoop -e constraint:node==master -e CLUSTER_NAME=bde-cluster --env-file=./hadoop.env bde2020/hadoop-namenode:1.0.0
+docker -H tcp://master:4000 run -d --name resourcemanager --net=bde_net -h resourcemanager.hadoop -e constraint:node==master --env-file=./hadoop.env bde2020/hadoop-resourcemanager:1.0.0
+docker -H tcp://master:4000 run -d --name historyserver -v /srv/hadoop/historyserver:/hadoop/dfs/name --net=bde_net -h historyserver.hadoop -e constraint:node==master --env-file=./hadoop.env bde2020/hadoop-historyserver:1.0.0
+docker -H tcp://master:4000 run -d --name nodemanager1 --net=hadoop -h nodemanager1.hadoop -e constraint:node==master  --env-file=./hadoop.env bde2020/hadoop-nodemanager:1.0.0
+docker -H tcp://master:4000 run -d --name datanode1 -v /srv/hadoop/datanode:/hadoop/dfs/data --net=bde_net -h datanode1.hadoop -e constraint:node==slave1 --env-file=./hadoop.env bde2020/hadoop-datanode:1.0.0
+docker -H tcp://master:4000 run -d --name datanode2 -v /srv/hadoop/datanode:/hadoop/dfs/data --net=bde_net -h datanode2.hadoop -e constraint:node==slave2 --env-file=./hadoop.env bde2020/hadoop-datanode:1.0.0
+docker -H tcp://master:4000 run -d --name datanode3 -v /srv/hadoop/datanode:/hadoop/dfs/data --net=bde_net -h datanode3.hadoop -e constraint:node==slave3 --env-file=./hadoop.env bde2020/hadoop-datanode:1.0.0
+docker -H tcp://master:4000 run -d --name datanode4 -v /srv/hadoop/datanode:/hadoop/dfs/data --net=bde_net -h datanode4.hadoop -e constraint:node==slave4 --env-file=./hadoop.env bde2020/hadoop-datanode:1.0.0
+cd ..
+rm -r docker-hadoop
