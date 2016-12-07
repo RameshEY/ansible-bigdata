3e37886bd99093feba64c2ba0046358df0ea5cca
Giannis Mouchakis <gmouchakis@iit.demokritos.gr>
2016-12-07 19:06:57 +0200

    added delay to configure network and cassandra

--- files/start_cassandra.sh
+++ files/start_cassandra.sh
@@ -2,8 +2,8 @@
 docker -H tcp://master:4000 run -d --name cassandra-cluster-1 -v /srv/cassandra:/var/lib/cassandra --net=bde_net -e constraint:node==slave1 bde2020/cassandra
 sleep 300
 docker -H tcp://master:4000 run -d --name cassandra-cluster-2 -v /srv/cassandra:/var/lib/cassandra --net=bde_net -e constraint:node==slave2 -e CASSANDRA_SEEDS="$(docker -H tcp://master:4000 inspect --format='{{ .NetworkSettings.Networks.bde_net.IPAddress }}' cassandra-cluster-1 | cut -d " " -f1)" bde2020/cassandra
-#sleep 200
+sleep 200
 docker -H tcp://master:4000 run -d --name cassandra-cluster-3 -v /srv/cassandra:/var/lib/cassandra --net=bde_net -e constraint:node==slave3 -e CASSANDRA_SEEDS="$(docker -H tcp://master:4000 inspect --format='{{ .NetworkSettings.Networks.bde_net.IPAddress }}' cassandra-cluster-1 | cut -d " " -f1)" bde2020/cassandra
-#sleep 200
+sleep 200
 docker -H tcp://master:4000 run -d --name cassandra-cluster-4 -v /srv/cassandra:/var/lib/cassandra --net=bde_net -e constraint:node==slave4 -e CASSANDRA_SEEDS="$(docker -H tcp://master:4000 inspect --format='{{ .NetworkSettings.Networks.bde_net.IPAddress }}' cassandra-cluster-1 | cut -d " " -f1)"  bde2020/cassandra
 
